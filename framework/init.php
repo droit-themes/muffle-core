21f4114d4557cc1e459a10145b7bc8b61365fc3a
omarfaruk2y2 <omarfaruk@droitlab.com>
2024-10-14 13:16:48 +0600

    update

--- framework/init.php
+++ framework/init.php
@@ -1,36 +1,43 @@
 <?php 
 
-//  add custom postype 
-$opt = get_option( 'muffle');
-$is_service_cpt = isset($opt['is_service_cpt']) ? $opt['is_service_cpt'] : '1';
-$slug = !empty($opt['service_slug']) ? strtolower(str_replace( ' ', '', $opt['service_slug'])) : $this->slug;
-//
-$is_project_cpt = isset($opt['is_project_cpt']) ? $opt['is_project_cpt'] : '1';
-$slug = !empty($opt['project_slug']) ? strtolower(str_replace( ' ', '', $opt['project_slug'])) : $this->slug;
-//
-$is_team_cpt = isset($opt['is_team_cpt']) ? $opt['is_team_cpt'] : '1';
-$slug = !empty($opt['team_slug']) ? strtolower(str_replace( ' ', '', $opt['team_slug'])) : $this->slug;
-//
+//  Add custom post type 
+$opt = get_option('muffle');
 
+// Define a default slug if not set
+$this->slug = isset($this->slug) ? $this->slug : 'default-slug';
+
+// Service CPT
+$is_service_cpt = isset($opt['is_service_cpt']) ? $opt['is_service_cpt'] : '1';
+$service_slug = !empty($opt['service_slug']) ? strtolower(str_replace(' ', '', $opt['service_slug'])) : $this->slug;
+
+// Project CPT
+$is_project_cpt = isset($opt['is_project_cpt']) ? $opt['is_project_cpt'] : '1';
+$project_slug = !empty($opt['project_slug']) ? strtolower(str_replace(' ', '', $opt['project_slug'])) : $this->slug;
+
+// Team CPT
+$is_team_cpt = isset($opt['is_team_cpt']) ? $opt['is_team_cpt'] : '1';
+$team_slug = !empty($opt['team_slug']) ? strtolower(str_replace(' ', '', $opt['team_slug'])) : $this->slug;
+
+// Create custom post types and taxonomies
 $custom_postype = new \Dt_custom_postype\Dt_CustomPosttype;
 $taxonomy = new \Dt_custom_postype\Dt_Taxonomies;
-if ( $is_service_cpt == '1' ) {
-$custom_postype->dt_postype('service', 'Service', 'Services', array('title', 'editor', 'author', 'thumbnail', 'excerpt'));
-$taxonomy->dt_taxonomy( 'Type', 'Type', 'Types', 'service');
+
+if ($is_service_cpt == '1') {
+    $custom_postype->dt_postype('service', 'Service', 'Services', ['title', 'editor', 'author', 'thumbnail', 'excerpt']);
+    $taxonomy->dt_taxonomy('Type', 'Type', 'Types', 'service');
 }
 
-if ( $is_project_cpt == '1' ) {
-$custom_postype->dt_postype('project', 'project', 'Our Projects', array('title', 'editor', 'author', 'thumbnail', 'excerpt'));
-$taxonomy->dt_taxonomy( 'project_type', 'project Type', 'project Type', 'project');
+if ($is_project_cpt == '1') {
+    $custom_postype->dt_postype('project', 'Project', 'Our Projects', ['title', 'editor', 'author', 'thumbnail', 'excerpt']);
+    $taxonomy->dt_taxonomy('project_type', 'Project Type', 'Project Types', 'project');
 }
 
-if ( $is_team_cpt == '1' ) {
-$custom_postype->dt_postype('Team', 'Team', 'Our Team', array('title', 'editor', 'author', 'thumbnail', 'excerpt'));
+if ($is_team_cpt == '1') {
+    $custom_postype->dt_postype('team', 'Team', 'Our Team', ['title', 'editor', 'author', 'thumbnail', 'excerpt']);
 }
-
 
 // Register Menu Page
-add_action( 'admin_menu', 'register_admin_page_for_header_footer' );
+add_action('admin_menu', 'register_admin_page_for_header_footer');
 function register_admin_page_for_header_footer() {
     add_menu_page(
         'Roofy Header Footer',
@@ -43,13 +50,12 @@ function register_admin_page_for_header_footer() {
     );
 }
 
-$custom_postype->dt_postype('header', 'Header', 'Headers', array( 'elementor'), 'roofy-header-footer');
-$custom_postype->dt_postype('footer', 'Footer', 'Footers', array( 'elementor'), 'roofy-header-footer');
+// Add post types for header and footer
+$custom_postype->dt_postype('header', 'Header', 'Headers', ['elementor'], 'roofy-header-footer');
+$custom_postype->dt_postype('footer', 'Footer', 'Footers', ['elementor'], 'roofy-header-footer');
 
-//  Get header footer id
-
+// Get header/footer template ID
 function header_footer_template_id($postype, $post_id) {
-
     $template_id = '';
     $header_arg = [
         'post_type' => $postype,
@@ -61,39 +67,33 @@ function header_footer_template_id($postype, $post_id) {
     $headers = get_posts($header_arg);
 
     foreach ($headers as $header) {
-
-        if ( !get_post_meta( $header->ID, 'active_header', true  ) ) {
+        if (!get_post_meta($header->ID, 'active_header', true)) {
             continue;
         }
 
-        if ( get_post_meta($header->ID, 'entry_site', true) ) {
+        if (get_post_meta($header->ID, 'entry_site', true)) {
             $template_id = $header->ID;
-
         } elseif (get_post_meta($header->ID, 'select_page_menu', true)) {
-
-            if(in_array($post_id, get_post_meta($header->ID, 'select_page_menu', true))){
+            if (in_array($post_id, get_post_meta($header->ID, 'select_page_menu', true))) {
                 $template_id = $header->ID;
             }
-
         } else {
             $template_id = '';
         }
     }
+
     return $template_id;
 }
-//  add by default postype
 
+// Add default post types for Elementor support
 function roofy_header_footer_add_cpt_support() {
+    // if exists, assign to $cpt_support var
+    $cpt_support = get_option('elementor_cpt_support');
 
-	//if exists, assign to $cpt_support var
-	$cpt_support = get_option( 'elementor_cpt_support' );
-
-	//check if option DOESN'T exist in db
-	if( ! $cpt_support ) {
-		$cpt_support = [ 'header', 'footer']; //create array of our default supported post types
-		update_option( 'elementor_cpt_support', $cpt_support ); //write it to the database
-	}
-	
+    // check if option DOESN'T exist in db
+    if (!$cpt_support) {
+        $cpt_support = ['header', 'footer']; // create array of our default supported post types
+        update_option('elementor_cpt_support', $cpt_support); // write it to the database
+    }
 }
-add_action( 'after_switch_theme', 'roofy_header_footer_add_cpt_support' );
-
+add_action('after_switch_theme', 'roofy_header_footer_add_cpt_support');
